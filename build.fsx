4ad0581b82bb840f42197d378e4f9aa4f5452c8d
Janno Põldma <janno.poldma@gmail.com>
2018-05-09 10:08:28 +0300

    Fake 5 legacy

--- build.fsx
+++ build.fsx
@@ -1,7 +1,11 @@
 #r @"packages/build/FAKE/tools/FakeLib.dll"
 #r @"packages/build/BouncyCastle/lib/BouncyCastle.Crypto"
 
-open Fake
+open Fake.Core
+open Fake.Core.TargetOperators
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.FileSystemOperators
 open Org.BouncyCastle.Crypto
 open Org.BouncyCastle.Crypto.Generators
 open Org.BouncyCastle.Crypto.Operators
@@ -22,63 +26,74 @@ let [<Literal>] SubjectName = "CN=localhost"
 let srcDir = __SOURCE_DIRECTORY__ </> "src"
 let certificatePath = srcDir </> (sprintf "%s.pfx" ApplicationName)
 
-let serverPath = "./src/Server" |> FullName
-let clientPath = "./src/Client" |> FullName
-let deployDir = "./deploy" |> FullName
+let serverPath = "./src/Server" |> Path.getFullName
+let clientPath = "./src/Client" |> Path.getFullName
+let deployDir = "./deploy" |> Path.getFullName
 
 let platformTool tool winTool =
-    let tool = if isUnix then tool else winTool
+    let tool = if Environment.isUnix then tool else winTool
     tool
-    |> ProcessHelper.tryFindFileOnPath
+    |> Process.tryFindFileOnPath
     |> function Some t -> t | _ -> failwithf "%s not found" tool
 
 let nodeTool = platformTool "node" "node.exe"
 let yarnTool = platformTool "yarn" "yarn.cmd"
 
-let dotnetcliVersion = DotNetCli.GetDotNetSDKVersionFromGlobalJson()
-let mutable dotnetCli = "dotnet"
+let dotnetcliVersion = DotNet.getSDKVersionFromGlobalJson()
+
+let install =
+    lazy DotNet.install (fun opt -> { opt with Version = DotNet.Version dotnetcliVersion})
+
+let inline withWorkingDir wd =
+    DotNet.Options.lift install.Value
+    >> DotNet.Options.withWorkingDirectory wd
 
 let run cmd args workingDir =
     let result =
-        ExecProcess (fun info ->
-            info.FileName <- cmd
-            info.WorkingDirectory <- workingDir
-            info.Arguments <- args) TimeSpan.MaxValue
+        Process.execSimple
+            (fun info ->
+                { info with
+                    FileName = cmd
+                    WorkingDirectory = workingDir
+                    Arguments = args })
+            TimeSpan.MaxValue
     if result <> 0 then failwithf "'%s %s' failed" cmd args
 
-Target "Clean" (fun _ -> 
-    CleanDirs [deployDir]
+Target.create "Clean" (fun _ -> 
+    Shell.cleanDirs [deployDir]
 )
 
-Target "InstallDotNetCore" (fun _ ->
-    dotnetCli <- DotNetCli.InstallDotNetSDK dotnetcliVersion
-)
-
-Target "InstallClient" (fun _ ->
+Target.create "InstallClient" (fun _ ->
     printfn "Node version:"
     run nodeTool "--version" __SOURCE_DIRECTORY__
     printfn "Yarn version:"
     run yarnTool "--version" __SOURCE_DIRECTORY__
     run yarnTool "install --frozen-lockfile" __SOURCE_DIRECTORY__
-    run dotnetCli "restore" clientPath
+    DotNet.restore (withWorkingDir clientPath) ""
 )
 
-Target "RestoreServer" (fun () -> 
-    run dotnetCli "restore" serverPath
+Target.create "RestoreServer" (fun _ ->
+    DotNet.restore (withWorkingDir serverPath) ""
 )
 
-Target "Build" (fun () ->
-    run dotnetCli "build" serverPath
-    run dotnetCli "fable webpack -- -p" clientPath
+Target.create "Build" (fun _ ->
+    DotNet.build (withWorkingDir serverPath) ""
+    let result = DotNet.exec (withWorkingDir clientPath) "fable" "webpack -- -p"
+    if not result.OK then
+        failwithf "'fable webpack' failed with errors %A." result.Errors
 )
 
-Target "Run" (fun () ->
+Target.create "Run" (fun _ ->
     let server = async {
-        setEnvironVar "ASPNETCORE_ENVIRONMENT" "Development"
-        run dotnetCli "watch run" serverPath
+        Environment.setEnvironVar "ASPNETCORE_ENVIRONMENT" "Development"
+        let result = DotNet.exec (withWorkingDir serverPath) "watch" "run"
+        if not result.OK then
+            failwithf "'watch run' failed with errors %A." result.Errors
     }
     let client = async {
-        run dotnetCli "fable start" clientPath
+        let result = DotNet.exec (withWorkingDir clientPath) "fable" "start"
+        if not result.OK then
+            failwithf "'fable start' failed with errors %A." result.Errors
     }
     let browser = async {
         Threading.Thread.Sleep 5000
@@ -91,7 +106,7 @@ Target "Run" (fun () ->
     |> ignore
 )
 
-Target "GenerateCertificate" (fun () ->
+Target.create "GenerateCertificate" (fun _ ->
     let randomGenerator = CryptoApiRandomGenerator()
     let random = SecureRandom(randomGenerator)
 
@@ -137,7 +152,6 @@ Target "GenerateCertificate" (fun () ->
 )
 
 "Clean"
-    ==> "InstallDotNetCore"
     ==> "InstallClient"
     ==> "Build"
 
@@ -145,4 +159,4 @@ Target "GenerateCertificate" (fun () ->
     ==> "RestoreServer"
     ==> "Run"
 
-RunTargetOrDefault "Build"
+Target.runOrDefault "Build"
